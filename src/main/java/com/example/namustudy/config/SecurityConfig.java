1f9017f34b3bde6209d23a482184ba535e0cc1c5
namugocps <namugocps@gmail.com>
2022-12-25 21:08:43 +0900

    나무스터디 웹 사이트 #4 - 스프링 시큐리티 설정

---
+++ src/main/java/com/example/namustudy/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.namustudy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter { // 상속 받으면 손 쉽게 설정 가능
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        // 특정 요청은 시큐리티 체크 안하도록 설정
+        http.authorizeRequests()
+                .mvcMatchers("/","/login","/sign-up","/check-email","/check-email-token","/email-login","/check-email-login","/login-link")
+                .permitAll().mvcMatchers(HttpMethod.GET,"/profile/*").permitAll().anyRequest().authenticated();
+    }
+}
