bf4cc8062ec77240a038d6e2d0feab9558ca0a7a
namugocps <namugocps@gmail.com>
2023-01-14 21:08:22 +0900

    나무스터디 웹 사이트 #9 - 회원 가입 폼 서브밋 검증

--- src/main/java/com/example/namustudy/account/AccountController.java
+++ src/main/java/com/example/namustudy/account/AccountController.java
@@ -2,7 +2,12 @@ package com.example.namustudy.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
 
 @Controller
 public class AccountController {
@@ -12,4 +17,14 @@ public class AccountController {
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
+        if(errors.hasErrors()){
+            return "account/sign-up";
+        }
+        // TODO 회원 가입 처리
+        return "redirect:/";
+    }
+
 }
