cefe251f65f3654f0c2f8cdcca64366aa4531d41
namugocps <namugocps@gmail.com>
2023-01-15 21:20:20 +0900

    나무스터디 웹 사이트 #10 - 회원 가입 폼 서브밋 검증

--- src/main/java/com/example/namustudy/account/AccountController.java
+++ src/main/java/com/example/namustudy/account/AccountController.java
@@ -1,16 +1,27 @@
 package com.example.namustudy.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
@@ -23,6 +34,8 @@ public class AccountController {
         if(errors.hasErrors()){
             return "account/sign-up";
         }
+
+        signUpFormValidator.validate(signUpForm, errors);
         // TODO 회원 가입 처리
         return "redirect:/";
     }
