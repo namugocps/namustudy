1ced8d2e280de7385b071a85517ecd7b785de65c
namugocps <namugocps@gmail.com>
2023-03-12 22:23:17 +0900

    나무스터디 웹 사이트 #64 - 현재 인증된 사용자 정보 참조

---
+++ src/main/java/com/example/namustudy/main/MainController.java
@@ -0,0 +1,16 @@
+package com.example.namustudy.main;
+
+import com.example.namustudy.account.CurrentUser;
+import com.example.namustudy.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model){
+
+    }
+}
