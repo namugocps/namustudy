95ac10b404808b502bc67d382d3315d068a957ba
namugocps <namugocps@gmail.com>
2023-04-08 21:33:29 +0900

    나무스터디 웹 사이트 #91 - 로그인 로그아웃 테스트

--- src/test/java/com/example/namustudy/main/MainControllerTest.java
+++ src/test/java/com/example/namustudy/main/MainControllerTest.java
@@ -9,7 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -30,9 +32,11 @@ class MainControllerTest {
         accountService.processNewAccount(signUpForm);
 
         mockMvc.perform(post("login")
-                .param("username","seokwon")
-                .param("password","12345678"))
-                .andExpect(status().is3xxRedirection());
+                .param("username","seokwon@email.com")
+                .param("password","12345678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
         
     }
 }
