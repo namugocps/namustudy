de854a5bf5e40be16c0321e292bf1a94fcd80ef4
namugocps <namugocps@gmail.com>
2023-04-13 23:17:40 +0900

    나무스터디 웹 사이트 #96 - 로그인 로그아웃 테스트

--- src/test/java/com/example/namustudy/main/MainControllerTest.java
+++ src/test/java/com/example/namustudy/main/MainControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -82,6 +83,7 @@ class MainControllerTest {
                 .andExpect(unauthenticated());
     }
 
+    @WithMockUser
     @DisplayName("로그아웃")
     @Test
     void logout() throws Exception{
