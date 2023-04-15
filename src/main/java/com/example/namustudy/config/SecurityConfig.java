719afc6df2b87f66ae510574f748c9752f443682
namugocps <namugocps@gmail.com>
2023-04-15 22:29:09 +0900

    나무스터디 웹 사이트 #98 - 로그인 기억하기

--- src/main/java/com/example/namustudy/config/SecurityConfig.java
+++ src/main/java/com/example/namustudy/config/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.example.namustudy.config;
 
+import com.example.namustudy.account.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter { // 상속 받으면 손 쉽게 설정 가능
+
+    private final AccountService accountService;
+    private final DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -24,7 +35,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { // 상속 받
 
         http.logout()
                 .logoutSuccessUrl("/");
+
+        http.rememberMe()
+                .userDetailsService(accountService)
+                .tokenRepository(tokenRepository());
     }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository(){
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }
+
 
     @Override
     public void configure(WebSecurity web) throws Exception{
