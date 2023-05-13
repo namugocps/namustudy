0829148928b708eae3591a14e1c4edcb002115b4
DESKTOP\namug <namugocps@gmail.com>
2023-05-13 23:02:51 +0900

    나무스터디 웹 사이트 #125 - 프로필 뷰

---
+++ src/main/java/com/example/namustudy/settings/Profile.java
@@ -0,0 +1,15 @@
+package com.example.namustudy.settings;
+
+import lombok.Data;
+
+@Data
+public class Profile {
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+}
--- src/main/java/com/example/namustudy/settings/SetiingsController.java
+++ src/main/java/com/example/namustudy/settings/SetiingsController.java
@@ -1,4 +1,17 @@
 package com.example.namustudy.settings;
 
+import com.example.namustudy.account.CurrentUser;
+import com.example.namustudy.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SetiingsController {
+
+    @GetMapping("/settings/profile")
+    public String profileUpdateForm(@CurrentUser Account account, Model model){
+        model.addAttribute(account);
+        return "profile";
+    }
 }
