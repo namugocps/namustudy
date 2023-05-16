c70c2dfbda25a2fe49da3a415bee08446697238e
DESKTOP\namug <namugocps@gmail.com>
2023-05-16 23:13:37 +0900

    나무스터디 웹 사이트 #128 - 프로필 수정 폼

--- src/main/java/com/example/namustudy/settings/SetiingsController.java
+++ src/main/java/com/example/namustudy/settings/SetiingsController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SetiingsController {
 
     @GetMapping("/settings/profile")
-    public void profileUpdateForm(@CurrentUser Account account, Model model){
+    public String profileUpdateForm(@CurrentUser Account account, Model model){
         model.addAttribute(account);
         model.addAttribute(new Profile(account));
+        return "settings/profile";
     }
 }
