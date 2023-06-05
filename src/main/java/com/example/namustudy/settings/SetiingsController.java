057b503fe1cc8bf979e8fcb2bee518e0fe28222d
DESKTOP\namug <namugocps@gmail.com>
2023-06-05 21:46:08 +0900

    나무스터디 웹 사이트 #148 - 프로필 수정 처리

--- src/main/java/com/example/namustudy/settings/SetiingsController.java
+++ src/main/java/com/example/namustudy/settings/SetiingsController.java
@@ -21,7 +21,7 @@ public class SetiingsController {
 
     private final AccountService accountService;
 
-    @GetMapping("/settings/profile")
+    @GetMapping(SETTINGS_PROFILE_URL)
     public String profileUpdateForm(@CurrentUser Account account, Model model){
         model.addAttribute(account);
         model.addAttribute(new Profile(account));
