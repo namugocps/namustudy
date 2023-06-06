91548d4630b29f15d7cbe2993567a678b7c9da8e
DESKTOP\namug <namugocps@gmail.com>
2023-06-06 23:24:43 +0900

    나무스터디 웹 사이트 #149 - 프로필 수정 처리

--- src/main/java/com/example/namustudy/settings/Profile.java
+++ src/main/java/com/example/namustudy/settings/Profile.java
@@ -2,8 +2,10 @@ package com.example.namustudy.settings;
 
 import com.example.namustudy.domain.Account;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Profile {
 
     private String bio;
