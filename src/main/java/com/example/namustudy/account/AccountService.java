525b75f70b830bf6f030f3db993a608601b2b719
DESKTOP\namug <namugocps@gmail.com>
2023-06-08 22:38:51 +0900

    나무스터디 웹 사이트 #151 - 프로필 수정 처리

--- src/main/java/com/example/namustudy/account/AccountService.java
+++ src/main/java/com/example/namustudy/account/AccountService.java
@@ -91,5 +91,6 @@ public class AccountService implements UserDetailsService {
         account.setOccupation(profile.getOccupation());
         account.setLocation(profile.getLocation());
         account.setBio(profile.getBio());
+        accountRepository.save(account);
     }
 }
