08c6bb2e5ab93e5a5d84a8760616a54ab358b9b6
DESKTOP\namug <namugocps@gmail.com>
2023-06-09 22:04:32 +0900

    나무스터디 웹 사이트 #152 - 프로필 수정 처리

--- src/main/java/com/example/namustudy/settings/SetiingsController.java
+++ src/main/java/com/example/namustudy/settings/SetiingsController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -29,13 +30,15 @@ public class SetiingsController {
     }
 
     @PostMapping("/settings/profile")
-    public String updateProfile(@CurrentUser Account account, @Valid Profile profile, Errors errors, Model model){
+    public String updateProfile(@CurrentUser Account account, @Valid Profile profile, Errors errors, Model model,
+                                RedirectAttributes attributes){
         if(errors.hasErrors()){
             model.addAttribute(account);
             return SETTINGS_PROFILE_VIEW_NAME;
         }
 
         accountService.updateProfile(account, profile);
+        attributes.addFlashAttribute("message","프로필을 수정했습니다.");
         return "redirect:" +SETTINGS_PROFILE_URL;
     }
 }
