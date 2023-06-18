b3c7efe1228a70f034727d6d7701f4d150215505
DESKTOP\namug <namugocps@gmail.com>
2023-06-18 21:57:39 +0900

    나무스터디 웹 사이트 #161 - 프로필 수정 처리

--- src/test/java/com/example/namustudy/settings/SetiingsControllerTest.java
+++ src/test/java/com/example/namustudy/settings/SetiingsControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,6 +20,7 @@ class SetiingsControllerTest {
     @DisplayName("프로필 수정하기 - 입력값 정상")
     @Test
     void updateProfile() throws Exception{
-        
+        mockMvc.perform(post(SetiingsController.SETTINGS_PROFILE_URL)
+                .param("bio", "짧은 소개를 수정하는 경우."));
     }
 }
