bd85856ad99d889291fd695942ff64659325edda
DESKTOP\namug <namugocps@gmail.com>
2023-06-24 22:18:36 +0900

    나무스터디 웹 사이트 #167 - 프로필 수정 테스트

--- src/test/java/com/example/namustudy/settings/SetiingsControllerTest.java
+++ src/test/java/com/example/namustudy/settings/SetiingsControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,6 +29,7 @@ class SetiingsControllerTest {
     @Autowired
     AccountRepository accountRepository;
 
+    @WithUserDetails("kee")
     @DisplayName("프로필 수정하기 - 입력값 정상")
     @Test
     void updateProfile() throws Exception{
