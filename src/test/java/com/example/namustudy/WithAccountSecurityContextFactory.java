ba44c6a1d01e4d3aa0b09467143037b3ac79890e
DESKTOP\namug <namugocps@gmail.com>
2023-06-30 22:50:54 +0900

    나무스터디 웹 사이트 #173 - 프로필 수정 테스트

--- src/test/java/com/example/namustudy/WithAccountSecurityContextFactory.java
+++ src/test/java/com/example/namustudy/WithAccountSecurityContextFactory.java
@@ -1,9 +1,14 @@
 package com.example.namustudy;
 
+import com.example.namustudy.account.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
+@RequiredArgsConstructor
 public class WithAccountSecurityContextFactory implements WithSecurityContextFactory<WithAccount> {
+
+    private final AccountService accountService;
     @Override
     public SecurityContext createSecurityContext(WithAccount annotation) {
         return null;
