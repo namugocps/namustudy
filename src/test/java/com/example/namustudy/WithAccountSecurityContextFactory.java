44214befc89c9d0daefec14af51674072b6a51b7
DESKTOP\namug <namugocps@gmail.com>
2023-07-01 22:27:46 +0900

    나무스터디 웹 사이트 #174 - 프로필 수정 테스트

--- src/test/java/com/example/namustudy/WithAccountSecurityContextFactory.java
+++ src/test/java/com/example/namustudy/WithAccountSecurityContextFactory.java
@@ -2,7 +2,11 @@ package com.example.namustudy;
 
 import com.example.namustudy.account.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
 @RequiredArgsConstructor
@@ -10,7 +14,12 @@ public class WithAccountSecurityContextFactory implements WithSecurityContextFac
 
     private final AccountService accountService;
     @Override
-    public SecurityContext createSecurityContext(WithAccount annotation) {
-        return null;
+    public SecurityContext createSecurityContext(WithAccount withAccount) {
+        String nickname = withAccount.value();
+
+        UserDetails principal = accountService.loadUserByUsername(nickname);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(principal, principal.getPassword(), principal.getAuthorities());
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
     }
 }
