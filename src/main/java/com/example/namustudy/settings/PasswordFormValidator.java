602a91e9663dfe6b13c729870e6bdced11aad3c4
DESKTOP\namug <namugocps@gmail.com>
2023-08-07 22:07:47 +0900

    나무스터디 웹 사이트 #211 - 패스워드 수정

---
+++ src/main/java/com/example/namustudy/settings/PasswordFormValidator.java
@@ -0,0 +1,16 @@
+package com.example.namustudy.settings;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class PasswordFormValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+    }
+}
