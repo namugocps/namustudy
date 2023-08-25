4f2fb14f9c2a0d288f9efc9790226878c1c67eb5
DESKTOP\namug <namugocps@gmail.com>
2023-08-25 23:37:33 +0900

    나무스터디 웹 사이트 #229 - 패스워드 수정 테스트

--- src/test/java/com/example/namustudy/settings/SetiingsControllerTest.java
+++ src/test/java/com/example/namustudy/settings/SetiingsControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
@@ -31,6 +32,8 @@ class SetiingsControllerTest {
     
     @Autowired
     AccountRepository accountRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @AfterEach
     void afterEach(){
@@ -104,5 +107,8 @@ class SetiingsControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl(SetiingsController.SETTINGS_PASSWORD_URL))
                 .andExpect(flash().attributeExists("message"));
+
+        Account seokwon = accountRepository.findByNickname("seokwon");
+        assertTrue(passwordEncoder.matches("12345678",seokwon.getPassword());
     }
 }
