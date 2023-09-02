3634d8e8b948529a58f4dd685cc077c018601d6a
DESKTOP\namug <namugocps@gmail.com>
2023-09-02 23:44:37 +0900

    나무스터디 웹 사이트 #237 - 패스워드 수정 테스트

---
+++ src/main/java/com/example/namustudy/settings/Notifications.java
@@ -0,0 +1,11 @@
+package com.example.namustudy.settings;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Notifications {
+
+    private boolean studyCreatedByEamil;
+}
