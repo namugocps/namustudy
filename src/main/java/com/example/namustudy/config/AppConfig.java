534adca681d322f53fdc65443d8aed624f06fa75
DESKTOP\namug <namugocps@gmail.com>
2023-10-01 23:55:33 +0900

    나무스터디 웹 사이트 #266 - 알림 설정

--- src/main/java/com/example/namustudy/config/AppConfig.java
+++ src/main/java/com/example/namustudy/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.namustudy.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.NameTokenizers;
+import org.modelmapper.spi.NameTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -17,6 +19,8 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setDestinationNameTokenizer(NameTokenizers.CAMEL_CASE);
         return new ModelMapper();
     }
 }
