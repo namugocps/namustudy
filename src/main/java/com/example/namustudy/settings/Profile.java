013187aadab67dd614634c09a2fd8e6bd2fb5fbd
DESKTOP\namug <namugocps@gmail.com>
2023-10-07 21:42:23 +0900

    나무스터디 웹 사이트 #272 - 알림 설정

--- src/main/java/com/example/namustudy/settings/Profile.java
+++ src/main/java/com/example/namustudy/settings/Profile.java
@@ -21,12 +21,4 @@ public class Profile {
     private String location;
 
     private String profileImage;
-
-    public Profile(Account account){
-        this.bio = account.getBio();
-        this.url = account.getUrl();
-        this.occupation = account.getOccupation();
-        this.location = account.getLocation();
-        this.profileImage = account.getProfileImage();
-    }
 }
