5c298ebd8f6a83f9151f1dd132e5f20b0437fe2c
DESKTOP\namug <namugocps@gmail.com>
2023-10-18 22:13:28 +0900

    나무스터디 웹 사이트 #283 - 닉네임 수정

--- src/main/java/com/example/namustudy/settings/validator/NicknameValidator.java
+++ src/main/java/com/example/namustudy/settings/validator/NicknameValidator.java
@@ -4,15 +4,22 @@ import com.example.namustudy.account.AccountRepository;
 import com.example.namustudy.settings.form.NicknameForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
 @Component
 @RequiredArgsConstructor
-public class NicknameValidator {
+public class NicknameValidator implements Validator {
 
     private final AccountRepository accountRepository;
 
     @Override
     public boolean supports(Class<?> clazz){
         return NicknameForm.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors){
+        NicknameForm nicknameForm = (NicknameForm) target;
     }
 }
