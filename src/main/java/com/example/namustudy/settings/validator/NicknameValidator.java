d2c9b6ed7ad757f61facd34dff98c26b1969ff7e
DESKTOP\namug <namugocps@gmail.com>
2023-10-20 23:20:55 +0900

    나무스터디 웹 사이트 #285 - 닉네임 수정

--- src/main/java/com/example/namustudy/settings/validator/NicknameValidator.java
+++ src/main/java/com/example/namustudy/settings/validator/NicknameValidator.java
@@ -23,5 +23,8 @@ public class NicknameValidator implements Validator {
     public void validate(Object target, Errors errors){
         NicknameForm nicknameForm = (NicknameForm) target;
         Account byNickname = accountRepository.findByNickname((nicknameForm.getNickname()));
+        if(byNickname != null){
+            errors.rejectValue("nickname","wrong.value","입력하신 닉네임을 사용할 수 없습니다.");
+        }
     }
 }
