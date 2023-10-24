488579e68e16f8567dbaeb543996a5e086515258
DESKTOP\namug <namugocps@gmail.com>
2023-10-24 23:39:17 +0900

    나무스터디 웹 사이트 #289 - 닉네임 수정

--- src/main/java/com/example/namustudy/settings/SetiingsController.java
+++ src/main/java/com/example/namustudy/settings/SetiingsController.java
@@ -111,6 +111,6 @@ public class SetiingsController {
     public String updateAccountForm(@CurrentUser Account account, Model model){
         model.addAttribute(account);
         model.addAttribute(modelMapper.map(account, NicknameForm.class));
-        
+        return SETTINGS_ACCOUNT_VIEW_NAME;
     }
 }
