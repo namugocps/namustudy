a3a041f002a379bd12fcfbe5382d634f463122c2
DESKTOP\namug <namugocps@gmail.com>
2023-11-21 19:49:51 +0900

    나무스터디 웹 사이트 #317 - 닉네임 수정

--- src/main/java/com/example/namustudy/domain/Tag.java
+++ src/main/java/com/example/namustudy/domain/Tag.java
@@ -1,10 +1,15 @@
 package com.example.namustudy.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Tag {
 
     @Id
