854b87a9b9d9a213122d406e63e7def80b1db278
DESKTOP\namug <namugocps@gmail.com>
2024-01-05 18:48:57 +0900

    나무스터디 웹 사이트 #361 - 관심 주제 등록 뷰

--- src/main/java/com/example/namustudy/tag/TagRepository.java
+++ src/main/java/com/example/namustudy/tag/TagRepository.java
@@ -1,6 +1,10 @@
 package com.example.namustudy.tag;
 
+import com.example.namustudy.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface TagRepository extends JpaRepository {
+
+@Transactional(readOnly = true)
+public interface TagRepository extends JpaRepository<Tag, Long> {
 }
