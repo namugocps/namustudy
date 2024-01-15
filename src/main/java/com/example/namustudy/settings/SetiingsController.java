36551ff1439132206c8fd5fbbb7839a2bb87f847
DESKTOP\namug <namugocps@gmail.com>
2024-01-15 18:20:56 +0900

    나무스터디 웹 사이트 #369 - 관심 주제 등록 뷰

--- src/main/java/com/example/namustudy/settings/SetiingsController.java
+++ src/main/java/com/example/namustudy/settings/SetiingsController.java
@@ -128,15 +128,13 @@ public class SetiingsController {
     @PostMapping("/settings/tags/add")
     public String addTag(@CurrentUser Account account, @RequestBody TagForm tagForm){
         String title = tagForm.getTagTitle();
-//        Optional<Tag> tag = Optional.of(tagRepository.findByTitle(title).orElseGet(() -> tagRepository.save(Tag.builder()
-//                .title(tagForm.getTagTitle())
-//                .build())));
 
         Tag tag = tagRepository.findByTitle(title);
         if (tag == null){
             tag  = tagRepository.save(Tag.builder().title(tagForm.getTagTitle()).build());
         }
 
+        accountService.addTag(account, tag);
         return SETTINGS_TAGS_VIEW_NAME;
     }
 
