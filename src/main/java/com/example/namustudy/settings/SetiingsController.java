85354ad2201ce53b043eecc788f363576beff68f
DESKTOP\namug <namugocps@gmail.com>
2024-01-16 22:31:38 +0900

    나무스터디 웹 사이트 #370 - 관심 주제 등록 뷰

--- src/main/java/com/example/namustudy/settings/SetiingsController.java
+++ src/main/java/com/example/namustudy/settings/SetiingsController.java
@@ -15,10 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -126,6 +123,7 @@ public class SetiingsController {
     }
 
     @PostMapping("/settings/tags/add")
+    @ResponseBody
     public String addTag(@CurrentUser Account account, @RequestBody TagForm tagForm){
         String title = tagForm.getTagTitle();
 
