7c89dde29d8f0032dd921e2cb44335084085db43
DESKTOP\namug <namugocps@gmail.com>
2024-01-30 20:30:41 +0900

    나무스터디 웹 사이트 #383 - 관심 주제 등록 뷰

--- src/main/java/com/example/namustudy/settings/SetiingsController.java
+++ src/main/java/com/example/namustudy/settings/SetiingsController.java
@@ -22,6 +22,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -121,7 +122,8 @@ public class SetiingsController {
     @GetMapping(SETTINGS_TAGS_URL)
     public String updateTags(@CurrentUser Account account, Model model){
         model.addAttribute(account);
-        Set<Tag> tag =accountService.getTags(account);
+        Set<Tag> tags =accountService.getTags(account);
+        model.addAttribute("tags",tags.stream().map(Tag::getTitle).collect(Collectors.toList()));
 
         return SETTINGS_TAGS_VIEW_NAME;
     }
