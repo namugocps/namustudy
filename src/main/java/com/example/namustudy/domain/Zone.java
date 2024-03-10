f5d185fa629d279492db38cbbaa094aefd991b23
DESKTOP\namug <namugocps@gmail.com>
2024-03-10 22:22:09 +0900

    나무스터디 웹 사이트 #423 - 관심 주제 테스트

--- src/main/java/com/example/namustudy/domain/Zone.java
+++ src/main/java/com/example/namustudy/domain/Zone.java
@@ -1,4 +1,14 @@
 package com.example.namustudy.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
 public class Zone {
+
+
 }
