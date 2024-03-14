501474a8b4bf07d487d7f4890aa4f49a55225c0c
DESKTOP\namug <namugocps@gmail.com>
2024-03-14 21:55:43 +0900

    나무스터디 웹 사이트 #427 - 관심 주제 테스트

--- src/main/java/com/example/namustudy/domain/Zone.java
+++ src/main/java/com/example/namustudy/domain/Zone.java
@@ -2,6 +2,7 @@ package com.example.namustudy.domain;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -15,10 +16,13 @@ public class Zone {
     @GeneratedValue
     private Long id;
 
+    @Column(nullable = false)
     private String city;
 
+    @Column(nullable = false)
     private String localNameOfCity;
 
+    @Column(nullable = true)
     private String province;
 
 }
