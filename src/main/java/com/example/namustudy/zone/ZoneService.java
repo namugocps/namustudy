5bcd7f219aa5e3f1cc9c41bea4902034b9e71070
DESKTOP\namug <namugocps@gmail.com>
2024-03-18 20:56:05 +0900

    나무스터디 웹 사이트 #431 - 관심 주제 테스트

--- src/main/java/com/example/namustudy/zone/ZoneService.java
+++ src/main/java/com/example/namustudy/zone/ZoneService.java
@@ -1,4 +1,12 @@
 package com.example.namustudy.zone;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class ZoneService {
 }
