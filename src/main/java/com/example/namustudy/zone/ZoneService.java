0b7d6c35c9d737ff3557b329508b402c30cf5a4e
DESKTOP\namug <namugocps@gmail.com>
2024-03-24 19:32:41 +0900

    나무스터디 웹 사이트 #437 - 관심 주제 테스트

--- src/main/java/com/example/namustudy/zone/ZoneService.java
+++ src/main/java/com/example/namustudy/zone/ZoneService.java
@@ -11,6 +11,7 @@ import javax.transaction.Transactional;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,11 +25,12 @@ public class ZoneService {
     public void initZoneData() throws IOException {
         if(zoneRepository.count() == 0){
             Resource resource = new ClassPathResource("zones_kr.csv");
-            Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8).stream()
+            List<Zone> zoneList = Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8).stream()
                     .map(line ->{
                         String[] split = line.split(",");
                         return Zone.builder().city(split[0]).localNameOfCity(split[1]).province(split[2]).build();
                     }).collect(Collectors.toList());
+
         }
         
     }
