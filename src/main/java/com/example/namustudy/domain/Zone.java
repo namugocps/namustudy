2538e7267ca2118db4648228e633f3c4e44bf08c
DESKTOP\namug <namugocps@gmail.com>
2024-03-27 22:14:09 +0900

    나무스터디 웹 사이트 #440 - 관심 주제 테스트

--- src/main/java/com/example/namustudy/domain/Zone.java
+++ src/main/java/com/example/namustudy/domain/Zone.java
@@ -25,4 +25,7 @@ public class Zone {
     @Column(nullable = true)
     private String province;
 
+    @Override
+    public String toString() {return String.format("%s(%s)/%s", city, localNameOfCity, province);}
+
 }
