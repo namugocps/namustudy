bd9f44ac9e4227951460845b126526c55b791400
DESKTOP\namug <namugocps@gmail.com>
2024-04-13 22:58:43 +0900

    나무스터디 웹 사이트 #457 - 지역 정보 추가 삭제 및 테스트

--- src/main/java/com/example/namustudy/settings/form/ZoneForm.java
+++ src/main/java/com/example/namustudy/settings/form/ZoneForm.java
@@ -1,8 +1,15 @@
 package com.example.namustudy.settings.form;
 
+import lombok.Data;
+
+@Data
 public class ZoneForm {
 
     private String zoneName;
 
     public String getCityName() { return zoneName.substring(0, zoneName.indexOf("("));}
+
+    public String getProvinceName(){
+        return zoneName.substring(zoneName.indexOf("/") +1);
+    }
 }
