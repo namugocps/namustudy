609b8202737032b3de7a7b3af78de2b2eee98703
DESKTOP\namug <namugocps@gmail.com>
2024-04-18 22:57:07 +0900

    나무스터디 웹 사이트 #462 - 지역 정보 추가 삭제 및 테스트

--- src/main/java/com/example/namustudy/zone/ZoneRepository.java
+++ src/main/java/com/example/namustudy/zone/ZoneRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ZoneRepository extends JpaRepositorytory<Zone, Long>{
+    Zone findByCityAndProvince(String cityName, String provinceName);
         int count();
 
     void saveAll(List<Zone> zoneList);
