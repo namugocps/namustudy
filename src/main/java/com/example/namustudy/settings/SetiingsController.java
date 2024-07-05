9a93166085c4524bf1757bbb14aa4f17ee859dd5
DESKTOP\namug <namugocps@gmail.com>
2024-07-05 23:19:10 +0900

    나무스터디 웹 사이트 #533 - 지역 정보 추가 삭제 및 테스트

--- src/main/java/com/example/namustudy/settings/SetiingsController.java
+++ src/main/java/com/example/namustudy/settings/SetiingsController.java
@@ -47,7 +47,6 @@ public class SetiingsController {
     static final String ZONES = "/zones";
     static final String ACCOUNT = "/account";
     static final String SETTINGS_PROFILE_URL = "/settings/profile";
-    
     private final AccountService accountService;
     private final ModelMapper modelMapper;
     private final NicknameValidator nicknameValidator;
